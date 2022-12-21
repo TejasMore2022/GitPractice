8fa9713d9531f62d2e81a06699ecc70e2a6420b9
prashinde55 <prashinde55@gmail.com>
2022-12-21 18:10:13 +0530

    pranv code

---
+++ src/gitpractice/Pravin.java
@@ -0,0 +1,14 @@
+package gitpractice;
+
+public class Pravin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void pranav() {
+		System.out.println("Data added by pranav");
+		
+	}
+
+}
