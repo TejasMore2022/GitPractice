6732434ab34dee771e4eacc41c0c47cb05f46026
Krishna-dev870 <kateashishsunil@gmail.com>
2022-12-21 18:13:53 +0530

    First commit

---
+++ src/gitpractice/AshishClass.java
@@ -0,0 +1,12 @@
+package gitpractice;
+
+public class AshishClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Above is the Ashih class");
+		System.out.println("Second line of code");
+
+	}
+
+}
