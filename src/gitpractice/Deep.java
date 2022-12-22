263003f55c65c7ccf582b85dc12dc66bf2ad85e4
Krishna-dev870 <kateashishsunil@gmail.com>
2022-12-22 17:45:10 +0530

    Ashish

--- src/gitpractice/Deep.java
+++ src/gitpractice/Deep.java
@@ -4,7 +4,11 @@ public class Deep {
 
 	public static void main(String[] args) {
 
-     System.out.println("first line by Deep");
+		System.out.println("first line by Deep");
+
+		System.out.println("Thi is the line by Ashish");
+		System.out.println("Hi guys");
+		
 
 	}
 
--- src/gitpractice/GitAccessToTeam.java
+++ src/gitpractice/GitAccessToTeam.java
@@ -4,6 +4,9 @@ public class GitAccessToTeam {
 
 	public static void main(String[] args) {
 		System.out.println("We are giving access to all team members");
+		
+		
+		System.out.println("This is the first line bY Ashish");
 
 	}
 
