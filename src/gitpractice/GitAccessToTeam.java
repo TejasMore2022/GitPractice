9cb17ad7f6194a3843d692d7d5d7b6f1dbb3d801
TejasMore2022 <tejasmore02@gmail.com>
2022-12-22 17:12:42 +0530

    code by tejas

--- src/gitpractice/GitAccessToTeam.java
+++ src/gitpractice/GitAccessToTeam.java
@@ -4,8 +4,15 @@ public class GitAccessToTeam {
 
 	public static void main(String[] args) {
 		System.out.println("We are giving access to all team members");
-		System.out.println(" Second line pranav");
 
+		
+		System.out.println(" Second line pranav");
+		
+		
+		System.out.println("This line by Tejas");
+
+		
+		
 	}
 
 }
