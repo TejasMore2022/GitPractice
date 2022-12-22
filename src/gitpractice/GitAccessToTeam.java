6d23c3bbf3512cdb374fe8909256e9da69820ea8
prashinde55 <prashinde55@gmail.com>
2022-12-22 17:07:41 +0530

    Second line pranav

--- src/gitpractice/GitAccessToTeam.java
+++ src/gitpractice/GitAccessToTeam.java
@@ -4,8 +4,8 @@ public class GitAccessToTeam {
 
 	public static void main(String[] args) {
 		System.out.println("We are giving access to all team members");
+		System.out.println(" Second line pranav");
 
 	}
-
 
 }
