b0abd5e5a116f19df8771a3bae93e000700f059d
Dip-Zagade <savyasachi583@gmail.com>
2022-12-22 17:24:03 +0530

    Upadated By deep

--- src/gitpractice/Deep.java
+++ src/gitpractice/Deep.java
@@ -1,11 +1,18 @@
 package gitpractice;
 
 public class Deep {
+	
+	public void sample()
+	{
+		System.out.println("sample method");
+	}
 
 	public static void main(String[] args) {
 
      System.out.println("first line by Deep");
-
+     System.out.println("second line by Deep");
+     
+     
 	}
 
 }
